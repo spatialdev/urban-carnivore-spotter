49c50af9bcde85e56fd7f020c37ee7c68a6af7a6
Savannah Ostrowski <savannahostrowski@gmail.com>
2019-03-04 12:47:59 -0800

    Update to Form component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import ListView from '../components/ListView';
 import ReportWizard from '../components/ReportWizard';
-import ReportViewer from '../components/ReportViewer'
-
-
+import ReportViewer from '../components/ReportViewer';
 
 const Main = () => (
   <main>
     <Switch>
-      <Route exact path="/map" component={Map}/>
-      <Route exact path="/reports/view/:reportId" component={ReportViewer} />
+      {/*<Route exact path="/map" component={Map}/>*/}
+      {/*<Route exact path="/reports/view/:reportId" component={ReportViewer} />*/}
       <Route exact path="/reports/create" component={ReportWizard}/>
-      <Route exact path="/list"  component={ListView} />
+      {/*<Route exact path="/list"  component={ListView} />*/}
     </Switch>
   </main>
 );
-  export default Main;
+export default Main;
