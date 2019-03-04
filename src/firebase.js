244f6edb8942b7a40113f14698797332edffbd7d
Savannah Ostrowski <savannahostrowski@gmail.com>
2019-03-04 08:03:42 -0800

    Add components and .env

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import app from 'firebase/app';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// Initialize Firebase
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
+};
+
+export default class Firebase {
+  constructor() {
+    app.initializeApp(config);
+  }
+}
