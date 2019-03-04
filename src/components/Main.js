860197e5491a74c2743bd02185ebfa9e52bc2ff0
Savannah Ostrowski <savannahostrowski@gmail.com>
2019-03-04 08:03:42 -0800

    Add components and .env

---
+++ src/components/Main.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+
+import ListView from '../components/ListView';
+import ReportWizard from '../components/ReportWizard';
+import ReportViewer from '../components/ReportViewer'
+
+
+
+const Main = () => (
+  <main>
+    <Switch>
+      <Route exact path="/map" component={Map}/>
+      <Route exact path="/reports/view/:reportId" component={ReportViewer} />
+      <Route exact path="/reports/create" component={ReportWizard}/>
+      <Route exact path="/list"  component={ListView} />
+    </Switch>
+  </main>
+);
+  export default Main;
