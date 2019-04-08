94e578cf582cf5be4c8634a4c5436b9aaf1a6430
Savannah Ostrowski <savannahostrowski@gmail.com>
2019-04-08 13:00:24 -0700

    Formatting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 
-import Header from "./components/Header";
+import Header from './components/Header';
 import Footer from './components/Footer';
 import Main from './components/Main';
 
