369fd8144513aedcfa8f7068acb1c0cf80fdb5fb
Micah Nacht <mjnacht@gmail.com>
2019-04-11 09:29:27 -0700

    List view (#7)
    * [List] add listView
    
    * Only look at time_seen
    
    * Address comments
    
    * Respond to PR comments
    
    * Loading circle looks better
    
    * PR comments

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Form from './Form';
 import Map from './Map';
+import ListView from './ListView';
 
 const Main = () => (
   <main>
     <Switch>
       <Route exact path="/" component={Map}/>
       <Route exact path="/reports/create" component={Form}/>
+      <Route exact path="/list" component={ListView}/>
     </Switch>
   </main>
 );
