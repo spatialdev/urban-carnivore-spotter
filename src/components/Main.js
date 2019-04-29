6a87ad1f0b427a77f957d0962f9f845826b0d85a
Savannah Ostrowski <savannahostrowski@gmail.com>
2019-04-29 13:21:55 -0700

    Form Viewer + General Style Updates (#11)
    * Fix of cloud functions; form viewer
    
    * Form viewer component
    
    * Rebase
    
    * Add forgotten colon
    
    * Fixed function
    
    * Add error handling to get for single report
    
    * Populate real form data
    
    * Remove const
    
    * Updates as per Sayana

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+
 import Form from './Form';
 import Map from './Map';
 import ListView from './ListView';
+import ReportViewer from "./ReportViewer";
 
 const Main = () => (
   <main className="Main">
@@ -10,6 +12,7 @@ const Main = () => (
       <Route exact path="/" component={Map}/>
       <Route exact path="/reports/create" component={Form}/>
       <Route exact path="/list" component={ListView}/>
+      <Route exact path="/reports/:id" component={ReportViewer}/>
     </Switch>
   </main>
 );
