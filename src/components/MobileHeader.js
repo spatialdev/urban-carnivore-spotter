fe2a532b9a60d1a10aca7b104373184a6fb1424a
Savannah Ostrowski <savannahostrowski@gmail.com>
2019-04-30 09:44:07 -0700

    Update style

--- src/components/MobileHeader.js
+++ src/components/MobileHeader.js
@@ -24,6 +24,7 @@ class MobileHeader extends Component {
 
   render() {
     const { history, location } = this.props;
+
     return (
       <div style={{ position: 'fixed', width: '100%', zIndex: 100 }}>
         <AppBar position="static" color="default">
