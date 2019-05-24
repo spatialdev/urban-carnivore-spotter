784608b3f9de9529421235a23e0790d55224e8b7
Micah Nacht <mjnacht@gmail.com>
2019-05-24 10:40:08 -0700

    Actually fix colors, and remove unused path

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Form from './Form';
-import FilterDrawer from './FilterDrawer';
 import ListView from './ListView';
 import MapView from "./MapView";
 import ReportViewer from "./ReportViewer";
@@ -12,7 +11,6 @@ const Main = () => (
     <Switch>
       <Route exact path="/" render={() => <MapView/>}/>
       <Route exact path="/reports/create" component={Form}/>
-      <Route exact path="/filters" component={FilterDrawer}/>
       <Route exact path="/list" component={ListView}/>
       <Route exact path="/reports/:id" component={ReportViewer}/>
     </Switch>
---
+++ src/services/ColorService.js
@@ -0,0 +1,8 @@
+import { Map } from 'immutable';
+
+const speciesColorMap = Map({'black bear':'#801e78','bobcat': '#498029','coyote': '#561480','cougar/mountain lion': '#802a00','raccoon': '#093c80','opossum': '#FFDC26','river otter': '#7083ff'});
+
+export const getColorForSpecies = (species) => {
+    const color = speciesColorMap.get(species.toLowerCase());
+    return color ? color : '#805b14';
+}
