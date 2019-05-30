1dd19d586268b2840bd9f2841b350a078ba39ad6
Micah Nacht <mjnacht@gmail.com>
2019-05-30 11:35:34 -0700

    Updates

---
+++ precache-manifest.26868ae5107f2715355938565308bc69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d072c98ddd4fbf53a57655e85ba877b",
+    "url": "/urban-carnivore-spotter/static/media/logo.8d072c98.png"
+  },
+  {
+    "revision": "f6133c6d2b9ee73b260d",
+    "url": "/urban-carnivore-spotter/static/js/runtime~main.b0fb83d2.js"
+  },
+  {
+    "revision": "374a4303b0804783f1b4",
+    "url": "/urban-carnivore-spotter/static/js/main.bcf3a8ba.chunk.js"
+  },
+  {
+    "revision": "ead68bc9951626d6d314",
+    "url": "/urban-carnivore-spotter/static/js/2.6479b1bf.chunk.js"
+  },
+  {
+    "revision": "374a4303b0804783f1b4",
+    "url": "/urban-carnivore-spotter/static/css/main.9f43a1da.chunk.css"
+  },
+  {
+    "revision": "ead68bc9951626d6d314",
+    "url": "/urban-carnivore-spotter/static/css/2.dc997d72.chunk.css"
+  },
+  {
+    "revision": "02043e96a7ef2f95b0fa7a9824d0f37f",
+    "url": "/urban-carnivore-spotter/index.html"
+  }
+];
