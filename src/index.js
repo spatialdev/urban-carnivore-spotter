6e98ac1ea5f276a222aaded67f8fc3a07748b833
Micah Nacht <mjnacht@gmail.com>
2019-05-30 13:30:24 -0700

    Make this easily deployable

--- src/index.js
+++ src/index.js
@@ -8,8 +8,9 @@ import { store } from './store/index';
 import './index.css';
 import App from './App';
 
+
 ReactDOM.render((
-    <BrowserRouter>
+    <BrowserRouter basename="urban-carnivore-spotter">
       <Provider store={store}>
         <FirebaseContext.Provider value={new Firebase()}>
           <App/>
