aff59d0908b4f75c36677cd9ed63fc1050193215
Micah Nacht <mjnacht@gmail.com>
2019-06-06 14:57:06 -0700

    Add a 404 page

---
+++ src/components/NotFound.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+import raccoon from '../assets/Raccoon.svg';
+import { withStyles } from '@material-ui/core/styles';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+const styles = {
+    oops: {
+        color: '#8acc25',
+        fontSize: '64px',
+    },
+
+    text: {
+        fontSize: 20,
+        marginLeft: '20%',
+        marginRight: '20%',
+    },
+    raccoon: {
+        position: 'fixed',
+        bottom: 0,
+        transform: 'translateX(-50%)',
+        zIndex: -1,
+        maxHeight: '40%',
+        minHeight: '15%',
+    },
+    raccoonMobile: {
+        position: 'fixed',
+        bottom: '10%',
+        transform: 'translateX(-50%)',
+        zIndex: -1,
+        maxHeight: '30%',
+        minHeight: '15%',
+    },
+    button: {
+        backgroundColor: '#8acc25',
+        color: 'white',
+    }
+};
+
+const NotFound = (props) => {
+    const { classes, history, isMobile } = props;
+    return <div>
+        <h1 className={classes.oops}><strong>oops!</strong></h1>
+        <h2>404 - Page not found</h2>
+        <p className={classes.text}>Sorry, the page you are looking for could not be found.</p>
+        <Button
+            variant="contained"
+            onClick={() => history.push('/')}
+            className={classes.button}
+            size="large">Back to Map</Button>
+        <br/>
+        <img src={raccoon} alt="Raccoon" className={isMobile ? classes.raccoonMobile : classes.raccoon}/>
+    </div>
+};
+
+const mapStateToProps = (state) => {
+    console.log(state);
+    return { isMobile: state.isMobile };
+};
+
+export default withRouter(withStyles(styles)(connect(mapStateToProps)(NotFound)));
