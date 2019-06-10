59dbf885276f285020398e05aca873ff5fe9c00d
Micah Nacht <mjnacht@gmail.com>
2019-06-10 16:22:41 -0700

    Styling and behavior on the popup

--- src/components/PointTooltip.js
+++ src/components/PointTooltip.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react';
 import { withStyles } from '@material-ui/core/styles';
-import {Link} from "react-router-dom";
-import {KeyboardArrowLeft} from "@material-ui/icons";
-import Button from "./ReportViewer";
+import { withRouter } from 'react-router-dom';
+import {KeyboardArrowRight} from "@material-ui/icons";
 
 const DEFAULT_IMAGE_URL = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/2010-brown-bear.jpg/200px-2010-brown-bear.jpg";
 
@@ -11,22 +10,44 @@ const timeToString = time => {
     return `${date.toDateString()} ${date.getHours()}:${date.getMinutes()}`;
 };
 
+const styles = {
+    allContent: {
+        display: 'flex',
+        height: 80,
+    },
+    caption: {
+        fontWeight: 'bold'
+    },
+    reportLink: {
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+    },
+    image: {
+        maxWidth: 40,
+        maxHeight: 40,
+        margin: 4
+    }
+};
 
 /* TODO: Should be updated with neighborhood info */
 class PointTooltip extends Component {
     render() {
-        const { report } = this.props;
-        return <div >
-            <img width = {40} src = {report.data.mediaPaths && report.data.mediaPaths.length>0 ? report.data.mediaPaths[0] : DEFAULT_IMAGE_URL}  />
-            <div className = "caption">{report.data.species}</div>
-            <div className = "caption">Date & Time:</div>
-            <div>{timeToString(report.data.timestamp)}</div>
-            <div className = "caption">Location: {report.data.mapLat},{report.data.mapLng}</div>
-            <li>
-                <Link to={`/reports/${report.id}`}>See Report</Link>
-            </li>
+        const { report, classes, history } = this.props;
+        return <div className={classes.allContent}>
+            <img className={classes.image} src = {report.data.mediaPaths && report.data.mediaPaths.length>0 ? report.data.mediaPaths[0] : DEFAULT_IMAGE_URL}  />
+            <div className={classes.info}>
+                <div><strong>{report.data.species}</strong></div>
+                <div><strong>Date & Time:</strong></div>
+                <div>{timeToString(report.data.timestamp)}</div>
+                <div><strong>Location:</strong> {report.data.neighborhood}</div>
+            </div>
+            <div className={classes.reportLink} onClick={() => history.push(`/reports/${report.id}`)}>
+                <KeyboardArrowRight/>
+            </div>
         </div>
     }
 };
 
-export default PointTooltip;
+export default withStyles(styles)(withRouter(PointTooltip));
