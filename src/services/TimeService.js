f82c42fc39ef5ab827eb00ae51216f596746679c
Micah Nacht <mjnacht@gmail.com>
2019-06-26 17:32:26 -0700

    add a colon

--- src/services/TimeService.js
+++ src/services/TimeService.js
@@ -20,7 +20,7 @@ export const jsDateToTimeString = (time) => {
     const date = new Date(time);
     const hours = date.getHours().toString().padStart(2, '0');
     const minutes = date.getMinutes().toString().padStart(2, '0');
-    return `${hours} ${minutes}`;
+    return `${hours}:${minutes}`;
   }
   return 'Unknown Time';
 };
