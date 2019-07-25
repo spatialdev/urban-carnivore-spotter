d757b5fc4cf4fc4cf12c94c9a80eeba1f42eaa45
Micah Nacht <mjnacht@gmail.com>
2019-07-25 10:41:33 -0700

    pull splash page into own component, update styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,12 +8,10 @@ import ReportViewer from "./ReportViewer";
 import NotFound from './NotFound';
 import ResourceCard from "./ResourceCard";
 import Resources from "./Resources";
-import {Dialog, DialogContent} from "@material-ui/core";
-import {withCookies} from "react-cookie";
+import SplashPage from "./SplashPage";
 
-const VISITED_BEFORE = "visitedBefore";
 
-const Main = (props) => (
+const Main = () => (
   <main className="Main">
     <Switch>
       <Route exact path="/" render={() => <MapView/>}/>
@@ -24,25 +22,8 @@ const Main = (props) => (
       <Route exact path="/resources/:species" component={ResourceCard}/>
       <Route component={NotFound}/>
     </Switch>
-    <Dialog open={!props.cookies.get(VISITED_BEFORE)}
-      onClose={() => props.cookies.set(VISITED_BEFORE, true)}>
-      <DialogContent>
-        <p>The Seattle Urban Carnivore project is a partnership between Woodland Park Zoo and Seattle University and aims to explore how mammalian carnivores live and interact with people across urban and suburban areas in the Seattle region.</p>
-        <p>The project focuses on the following species:</p>
-        <ul>
-          <li>Black Bear</li>
-          <li>Bobcat</li>
-          <li>Cougar / Mountain Lion</li>
-          <li>Coyote</li>
-          <li>Opossum</li>
-          <li>Raccoon</li>
-          <li>River Otter</li>
-        </ul>
-        <p>These are terrestrial (not marine) mammals in the taxonomic Order Carnivora (*except for opossums). Some of them have a carnivorous diet (eating other animals). Many of them, however, have an omnivorous diet, eating plants as well as animals.</p>
-        <p>You can use this observation form to submit observations of any of the above animals (or if you think you may have seen one of them, but aren’t sure!)</p>
-      </DialogContent>
-    </Dialog>
+    <SplashPage />
   </main>
 );
 
-export default withCookies(Main);
+export default Main;
