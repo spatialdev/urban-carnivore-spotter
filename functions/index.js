27bae3550acad51f70b81ca7e5b4aa2d8a0b4170
Micah Nacht <mjnacht@gmail.com>
2019-09-03 17:44:07 -0700

    only send the information we need

--- functions/index.js
+++ functions/index.js
@@ -100,6 +100,21 @@ buildQuery = (queryParams, collection) => {
   return initialQuery;
 };
 
+getRelevantData = (document) => {
+  const allData = document.data();
+  return {
+    timestamp: allData.timestamp,
+    species: allData.species,
+    neighborhood: allData.neighborhood,
+    confidence: allData.confidence,
+    time_submitted: allData.time_submitted,
+    id: allData.id,
+    mediaPaths: allData.mediaPaths,
+    mapLng: allData.mapLng,
+    mapLat: allData.mapLat
+  }
+};
+
 exports.getReports = functions.https.onRequest((req, res) => {
   return cors(req, res, () => {
     if (req.method !== 'GET') {
@@ -128,13 +143,13 @@ exports.getReports = functions.https.onRequest((req, res) => {
               const distance = turf.distance(from, to, options);
               // If distance is within 500 miles from the query lat long
               if (distance <= 500) {
-                items.push({ id: doc.id, data: doc.data() });
+                items.push({ id: doc.id, data: getRelevantData(doc) });
               }
             }
           });
         } else {
           snapshot.forEach(doc => {
-            items.push({ id: doc.id, data: doc.data() });
+            items.push({ id: doc.id, data: getRelevantData(doc) });
           });
         }
         return items.length === 0 ? res.status(200).send('No data!') : res.status(200).send(items);
