8e3774d026e90a83d33af1ab3dec667055823874
Mealear Khiev <mealear.khiev@critigen.com>
2020-11-03 11:28:47 -0800

    Adds FAQ to mobile footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import AddIcon from '@material-ui/icons/Add';
-import MapIcon from '@material-ui/icons/Map';
-import SettingsIcon from '@material-ui/icons/Settings';
-import Button from '@material-ui/core/Button';
-import Fab from '@material-ui/core/Fab';
-import { withStyles } from '@material-ui/core/styles';
-
+import React from "react";
+import AddIcon from "@material-ui/icons/Add";
+import MapIcon from "@material-ui/icons/Map";
+import HelpIcon from "@material-ui/icons/Help";
+import SettingsIcon from "@material-ui/icons/Settings";
+import Button from "@material-ui/core/Button";
+import Fab from "@material-ui/core/Fab";
+import { withStyles } from "@material-ui/core/styles";
+import FaqIcon from "../assets/FAQ.svg";
 import { withRouter } from "react-router";
 
-const styles = theme => ({
+const styles = (theme) => ({
   list: {
     width: 250,
   },
   fullList: {
-    width: '100%',
+    width: "100%",
   },
   fab: {
     margin: theme.spacing.unit,
@@ -40,45 +41,114 @@ const styles = theme => ({
   plusButton: {
     position: "relative",
     top: -50,
+    left: 70,
   },
   footerIcons: {
-    '& span': {
-      display: 'block'
+    marginTop: "0.75em",
+    "& span": {
+      display: "block",
     },
-    '& p': {
-      textTransform: 'capitalize',
-      margin: 0
-    }
-  }
+    "& p": {
+      textTransform: "capitalize",
+      margin: 0,
+    },
+  },
 });
 
 const Footer = (props) => {
-    const { classes, history } = props;
-    const isTacoma = history.location.pathname.indexOf('tacoma') !== -1;
+  const { classes, history } = props;
+  const isTacoma = history.location.pathname.indexOf("tacoma") !== -1;
 
-    return (
-      <div className={classes.footerIconDiv}>
-        <div className={classes.flexColumn}>
-          <Button className={classes.footerIcons} style={{ float: 'left', marginLeft: '50px', color: history.location.pathname==='/'? '#3411ff': 'grey' }} >
-            <MapIcon style={{ color: history.location.pathname==='/' || isTacoma? '#3411FF': 'grey' }} onClick={() => isTacoma? history.push('/tacoma'): history.push('/')}/>
-            <p>Explore</p>
-          </Button>
-        </div>
-        <div className={classes.flexColumn}>
-          <Fab style={{backgroundColor: '#8acc25'}} aria-label="Add" className={classes.plusButton} size={"large"} onClick={() => isTacoma ? history.push('/tacoma/reports/create'): history.push('/reports/create')}>
-            <AddIcon style={{color: '#FFFFFF'}}/>
-          </Fab>
-        </div>
-        <div className={classes.flexColumn}>
-          <Button className={classes.footerIcons} style={{ float: 'right', marginRight: '50px', color: history.location.pathname.includes('/resources')?
-                '#3411FF': 'grey' }}
-                  onClick={() => isTacoma? history.push('/tacoma/resources') : history.push('/resources')}>
-            <SettingsIcon style={{ color: history.location.pathname.includes('/resources')? '#3411FF': 'grey' }} />
-            <p>Resources</p>
-          </Button>
-        </div>
+  return (
+    <div className={classes.footerIconDiv}>
+      <div className={classes.flexColumn}>
+        <Button
+          className={classes.footerIcons}
+          style={{
+            float: "left",
+            marginLeft: "50px",
+            color: history.location.pathname === "/" ? "#3411ff" : "grey",
+          }}
+        >
+          <MapIcon
+            style={{
+              color:
+                history.location.pathname === "/" || isTacoma
+                  ? "#3411FF"
+                  : "grey",
+            }}
+            onClick={() =>
+              isTacoma ? history.push("/tacoma") : history.push("/")
+            }
+          />
+          <p>Explore</p>
+        </Button>
       </div>
-    );
+      <div className={classes.flexColumn}>
+        <Fab
+          style={{ backgroundColor: "#8acc25" }}
+          aria-label="Add"
+          className={classes.plusButton}
+          size={"large"}
+          onClick={() =>
+            isTacoma
+              ? history.push("/tacoma/reports/create")
+              : history.push("/reports/create")
+          }
+        >
+          <AddIcon style={{ color: "#FFFFFF" }} />
+        </Fab>
+      </div>
+      <div className={classes.flexColumn}>
+        <Button
+          className={classes.footerIcons}
+          style={{
+            float: "right",
+            marginRight: "50px",
+            color: history.location.pathname.includes("/resources")
+              ? "#3411FF"
+              : "grey",
+          }}
+          onClick={() =>
+            isTacoma
+              ? history.push("/tacoma/resources")
+              : history.push("/resources")
+          }
+        >
+          <SettingsIcon
+            style={{
+              color: history.location.pathname.includes("/resources")
+                ? "#3411FF"
+                : "grey",
+            }}
+          />
+          <p>Resources</p>
+        </Button>
+      </div>
+      <div className={classes.flexColumn}>
+        <Button
+          className={classes.footerIcons}
+          style={{
+            float: "right",
+            marginRight: "50px",
+            color: history.location.pathname.includes("/faq")
+              ? "#3411FF"
+              : "grey",
+          }}
+          onClick={() => history.push("/faq")}
+        >
+          <HelpIcon
+            style={{
+              color: history.location.pathname.includes("/faq")
+                ? "#3411FF"
+                : "grey",
+            }}
+          />
+          <p>FAQ</p>
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default withRouter(withStyles(styles)(Footer));
