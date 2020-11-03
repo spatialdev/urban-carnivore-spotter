859d5f25158ed7e1b3454d5324b6cc76045717c8
Mealear Khiev <mealear.khiev@critigen.com>
2020-11-02 17:47:09 -0800

    Adds FAQ tab on header

--- src/components/DesktopHeader.js
+++ src/components/DesktopHeader.js
@@ -1,28 +1,110 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
-import Sticky from 'react-sticky-fill';
-import AppBar from '@material-ui/core/AppBar';
-import '../App.css';
-import {connect} from "react-redux";
-import zooLogo from '../assets/logo.png';
-import tacomaLogo from '../assets/tacomaLogo.png'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withRouter } from "react-router-dom";
+import Sticky from "react-sticky-fill";
+import AppBar from "@material-ui/core/AppBar";
+import "../App.css";
+import { connect } from "react-redux";
+import zooLogo from "../assets/logo.png";
+import tacomaLogo from "../assets/tacomaLogo.png";
 
 class DesktopHeader extends Component {
-
   render() {
     const { history } = this.props;
-    const isTacoma = history.location.pathname.indexOf('tacoma') !== -1;
+    const isTacoma = history.location.pathname.indexOf("tacoma") !== -1;
     return (
-      <Sticky style={{top: 0, width: '100%', zIndex: 100}}>
+      <Sticky style={{ top: 0, width: "100%", zIndex: 100 }}>
         <AppBar position="static" className="appBar">
-            {isTacoma ?  <div className="logoContainer"><img className="logo2" src={tacomaLogo} alt={"Pt. Defiance Zoo logo"}/></div> :  <div className="logoContainer"><img className="logo" src={zooLogo} alt={"Woodland Park Zoo logo"}/></div>}
-          <h1 className={isTacoma? "headerTitle" : "headerTitle"} onClick={() => isTacoma ? history.push('/tacoma') : history.push('/')} style={{ cursor: 'pointer' }}>
+          {isTacoma ? (
+            <div className="logoContainer">
+              <img
+                className="logo2"
+                src={tacomaLogo}
+                alt={"Pt. Defiance Zoo logo"}
+              />
+            </div>
+          ) : (
+            <div className="logoContainer">
+              <img
+                className="logo"
+                src={zooLogo}
+                alt={"Woodland Park Zoo logo"}
+              />
+            </div>
+          )}
+          <h1
+            className={isTacoma ? "headerTitle" : "headerTitle"}
+            onClick={() =>
+              isTacoma ? history.push("/tacoma") : history.push("/")
+            }
+            style={{ cursor: "pointer" }}
+          >
             Carnivore Spotter
           </h1>
           <div className="nav">
-            <div id="explore" className="categories" onClick={() => isTacoma ? history.push('/tacoma') : history.push('/')}><h4 style={{textDecoration: history.location.pathname==='/' || history.location.pathname==='/tacoma' ? "underline":""}}>Explore</h4></div>
-            <div id="resources" className="categories" onClick={() => isTacoma ? history.push('/tacoma/resources'): history.push('/resources')}><h4 style={{textDecoration: history.location.pathname==='/resources' ||history.location.pathname==='/tacoma/resources'? "underline":""}}>Resources</h4>
+            <div
+              id="explore"
+              className="categories"
+              onClick={() =>
+                isTacoma ? history.push("/tacoma") : history.push("/")
+              }
+            >
+              <h4
+                style={{
+                  fontSize: "0.85em",
+                  paddingBottom: "1.5em",
+                  borderBottom:
+                    history.location.pathname === "/" ||
+                    history.location.pathname === "/tacoma"
+                      ? "2px solid rgba(2,2,30,0.85)"
+                      : "",
+                }}
+              >
+                Explore
+              </h4>
+            </div>
+            <div
+              id="resources"
+              className="categories"
+              onClick={() =>
+                isTacoma
+                  ? history.push("/tacoma/resources")
+                  : history.push("/resources")
+              }
+            >
+              <h4
+                style={{
+                  fontSize: "0.85em",
+                  paddingBottom: "1.5em",
+                  borderBottom:
+                    history.location.pathname === "/resources" ||
+                    history.location.pathname === "/tacoma/resources"
+                      ? "2px solid rgba(2,2,30,0.85)"
+                      : "",
+                }}
+              >
+                Resources
+              </h4>
+            </div>
+            <div
+              id="faq"
+              className="categories"
+              onClick={() =>
+                isTacoma ? history.push("/tacoma/faq") : history.push("/faq")
+              }
+            >
+              <h4
+                style={{
+                  fontSize: "0.85em",
+                  paddingBottom: "1.5em",
+                  borderBottom:
+                    history.location.pathname === "/faq"
+                      ? "2px solid rgba(2,2,30,0.85)"
+                      : "",
+                }}
+              >
+                FAQ
+              </h4>
             </div>
           </div>
         </AppBar>
@@ -37,7 +119,7 @@ DesktopHeader.propTypes = {
 
 const mapStateToProps = (state) => {
   return {
-    isMobile: state.isMobile
+    isMobile: state.isMobile,
   };
 };
 
