1c638030736d39f04aa547d988a1ff46ec63aee9
Mealear Khiev <mealear.khiev@critigen.com>
2020-11-03 16:27:02 -0800

    Display the “play” button only for video or audio (not on photos/multiple photos)

--- src/components/ReportViewer.js
+++ src/components/ReportViewer.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
 import axios from "axios";
 
 import Button from "@material-ui/core/Button";
@@ -7,17 +7,18 @@ import Card from "@material-ui/core/Card";
 import { CircularProgress } from "@material-ui/core";
 import { KeyboardArrowLeft } from "@material-ui/icons";
 
-import {jsDateToTimeString} from "../services/TimeService";
+import { jsDateToTimeString } from "../services/TimeService";
 
-import ImageGallery from 'react-image-gallery';
-import Placeholder from '../assets/placeholder.svg';
-import CardMedia from '@material-ui/core/CardMedia';
-import {connect} from "react-redux";
+import Placeholder from "../assets/placeholder.svg";
+import CardMedia from "@material-ui/core/CardMedia";
+import { connect } from "react-redux";
 import * as ReactGA from "react-ga";
 
-const getReport = 'https://us-central1-seattlecarnivores-edca2.cloudfunctions.net/getReport';
-const getTacomaReport = 'https://us-central1-seattlecarnivores-edca2.cloudfunctions.net/getTacomaReport';
-const videoFormats = ['.mov', '.mp4', '.webm', '.ogg', '.avi', '.wmv', '.mkv'];
+const getReport =
+  "https://us-central1-seattlecarnivores-edca2.cloudfunctions.net/getReport";
+const getTacomaReport =
+  "https://us-central1-seattlecarnivores-edca2.cloudfunctions.net/getTacomaReport";
+const videoFormats = [".mov", ".mp4", ".webm", ".ogg", ".avi", ".wmv", ".mkv"];
 
 class ReportViewer extends Component {
   state = {
@@ -26,88 +27,128 @@ class ReportViewer extends Component {
 
   componentDidMount() {
     ReactGA.pageview(window.location.pathname);
-    const { match: { params: { id } } } = this.props;
-    const path = window.location.pathname.includes('/reports/tacoma')? getTacomaReport : getReport;
-    axios.get( path + `?id=${id}`)
-      .then(report => {
+    const {
+      match: {
+        params: { id },
+      },
+    } = this.props;
+    const path = window.location.pathname.includes("/reports/tacoma")
+      ? getTacomaReport
+      : getReport;
+    axios
+      .get(path + `?id=${id}`)
+      .then((report) => {
         this.setState({ report: report.data });
       })
-      .catch(error => error);
+      .catch((error) => error);
   }
 
   renderGalleryItem(item) {
     return (
-      <div className='image-gallery-image'>
-        { item.isVideo ?
+      <div className="image-gallery-image">
+        {item.isVideo ? (
           <video width="100%" controls autoPlay>
             <source src={item.original} />
           </video>
-        :
+        ) : (
           <img
             src={item.original}
             alt={item.originalAlt}
             srcSet={item.srcSet}
             sizes={item.sizes}
             title={item.originalTitle}
+            style={{ width: "400px" }}
           />
-        }
+        )}
 
-        {
-          item.description &&
-            <span className='image-gallery-description'>
-              {item.description}
-            </span>
-        }
+        {item.description && (
+          <span className="image-gallery-description">{item.description}</span>
+        )}
       </div>
     );
   }
 
   render() {
-    const { history,isMobile } = this.props;
+    const { history, isMobile } = this.props;
     const { report } = this.state;
 
     if (!report) {
-      return <CircularProgress/>
+      return <CircularProgress />;
     }
 
     let media = [];
 
     if (report.mediaPaths !== undefined && report.mediaPaths.length > 0) {
-      report.mediaPaths.map(med => {
-        const fileExtensionPattern = /\.([0-9a-z]+)(?=[?#])|(\.)(?:[\w]+)$/gmi;
+      report.mediaPaths.map((med) => {
+        const fileExtensionPattern = /\.([0-9a-z]+)(?=[?#])|(\.)(?:[\w]+)$/gim;
         const extension = med.match(fileExtensionPattern)[0];
         const isVideo = videoFormats.includes(extension.toLowerCase());
-        media.push({ original: med, thumbnail: med, isVideo: isVideo, ext: extension });
-      })
+        media.push({
+          original: med,
+          thumbnail: med,
+          isVideo: isVideo,
+          ext: extension,
+        });
+      });
     }
+
     return (
-      <div className={isMobile ? "reportViewerMobile": "reportViewerDesktop"}>
+      <div className={isMobile ? "reportViewerMobile" : "reportViewerDesktop"}>
         <div className="buttonAndCardContainer">
           <div className="backToExploreContainer">
-          <Button className="backToExplore" onClick={() => history.goBack()}> <KeyboardArrowLeft/>Back</Button>
+            <Button className="backToExplore" onClick={() => history.goBack()}>
+              {" "}
+              <KeyboardArrowLeft />
+              Back
+            </Button>
           </div>
           <div>
-        <Card className="reportCard">
-          <div className= "reportViewerTitle">
-            <h4>{report.species.toUpperCase()}</h4>
+            <Card className="reportCard">
+              <div className="reportViewerTitle">
+                <h4>{report.species.toUpperCase()}</h4>
+              </div>
+              {media.length > 0 ? (
+                media.map((mediaItem) => this.renderGalleryItem(mediaItem))
+              ) : (
+                <CardMedia
+                  className="reportPlaceHolderImage"
+                  image={Placeholder}
+                />
+              )}
+              <div
+                style={{
+                  backgroundColor: "white",
+                  textAlign: "left",
+                  paddingLeft: "30px",
+                }}
+              >
+                <p>
+                  <strong>Date:</strong>{" "}
+                  {new Date(report.timestamp).toDateString()}
+                </p>
+                <p>
+                  <strong>Time of Sighting:</strong>{" "}
+                  {jsDateToTimeString(report.timestamp)}
+                </p>
+                <p>
+                  <strong>Neighborhood:</strong> {report.neighborhood}
+                </p>
+                <p>
+                  <strong>Confidence:</strong> {report.confidence}
+                </p>
+                <p style={{ lineHeight: ".5" }}>
+                  <strong>Number of Species:</strong>
+                </p>
+                <p style={{ lineHeight: ".5" }}>
+                  <strong>Adult: </strong> {report.numberOfAdultSpecies}
+                </p>
+                <p style={{ lineHeight: ".5" }}>
+                  <strong>Young: </strong> {report.numberOfYoungSpecies}
+                </p>
+              </div>
+            </Card>
           </div>
-          {media.length!==0 ?
-            <ImageGallery items={media}
-                          renderItem={this.renderGalleryItem}
-                          showBullets={true} showIndex={false}
-                          showThumbnails={false} showVideo={true}/> : <CardMedia  className="reportPlaceHolderImage" image={Placeholder}/>}
-          <div style={{ backgroundColor: 'white', textAlign: 'left', paddingLeft: '30px'}}>
-            <p><strong>Date:</strong> {new Date(report.timestamp).toDateString()}</p>
-            <p><strong>Time of Sighting:</strong> {jsDateToTimeString(report.timestamp)}</p>
-            <p><strong>Neighborhood:</strong> {report.neighborhood}</p>
-            <p><strong>Confidence:</strong> {report.confidence}</p>
-            <p style={{lineHeight:'.5'}}><strong>Number of Species:</strong></p>
-            <p style={{lineHeight:'.5'}}><strong>Adult: </strong> {report.numberOfAdultSpecies}</p>
-            <p style={{lineHeight:'.5'}}><strong>Young: </strong> {report.numberOfYoungSpecies}</p>
-          </div>
-        </Card>
         </div>
-      </div>
       </div>
     );
   }
@@ -115,7 +156,7 @@ class ReportViewer extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    isMobile: state.isMobile
+    isMobile: state.isMobile,
   };
 };
-export default withRouter((connect(mapStateToProps)(ReportViewer)));
+export default withRouter(connect(mapStateToProps)(ReportViewer));
