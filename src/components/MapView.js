443b2fad77ef7f885b51b556538472559df0b3d7
Mealear Khiev <mealear.khiev@critigen.com>
2020-11-13 17:35:37 -0800

    Fixes zoom controls styles

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -463,7 +463,10 @@ class MapView extends Component {
                     top: "auto",
                     left: "auto",
                     margin: ".5rem",
+                    marginTop: "3rem",
                     display: "flex",
+                    boxShadow:
+                      "0px 3px 5px -1px rgba(0,0,0,0.2), 0px 6px 10px 0px rgba(0,0,0,0.14), 0px 1px 18px 0px rgba(0,0,0,0.12)",
                   }}
                 />
               </div>
