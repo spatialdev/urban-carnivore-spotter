8aae44f15709e1d762e13dbee79b1f42815a6b67
Mealear Khiev <mealear.khiev@critigen.com>
2020-12-07 11:59:59 -0800

    Filters out unknown neighborhoods from neighborhoods list

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
 
 import DesktopHeader from "./components/DesktopHeader";
-import Header from './components/MobileHeader';
-import Footer from './components/Footer';
-import Main from './components/Main';
-import { connect } from 'react-redux';
-import { setMobile, updateAllNeighborhoods} from './store/actions';
-import NeighborhoodService from './services/NeighborhoodService';
-import * as ReactGA from 'react-ga';
+import Header from "./components/MobileHeader";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+import { connect } from "react-redux";
+import { setMobile, updateAllNeighborhoods } from "./store/actions";
+import NeighborhoodService from "./services/NeighborhoodService";
+import * as ReactGA from "react-ga";
 
-import './App.css';
+import "./App.css";
 
 const trackingId = process.env.REACT_APP_GA_TRACKING_ID;
 
 class App extends Component {
-
   componentDidMount() {
     // Initialize Google Analytics
     ReactGA.initialize(trackingId);
     ReactGA.pageview(window.location.pathname);
     this.checkIfMobile();
-    window.addEventListener('resize', this.checkIfMobile);
-    NeighborhoodService.getAllNeighborhoods()
-      .then(allNeighborhoods => updateAllNeighborhoods(allNeighborhoods));
+    window.addEventListener("resize", this.checkIfMobile);
+    NeighborhoodService.getAllNeighborhoods().then((allNeighborhoods) => {
+      const allKnownNeighborhoods = allNeighborhoods.filter(
+        (neighborhood) => neighborhood !== "Unknown"
+      );
+      return updateAllNeighborhoods(allKnownNeighborhoods);
+    });
   }
 
   componentWillUnmount() {
-    window.removeEventListener('resize', this.checkIfMobile);
+    window.removeEventListener("resize", this.checkIfMobile);
   }
 
   checkIfMobile = () => {
@@ -35,16 +38,27 @@ class App extends Component {
   };
 
   render() {
-    const {isMobile, history} = this.props;
+    const { isMobile, history } = this.props;
     // If we are on the mobile create reports page, show nothing. Otherwise, show either the mobile header or the
     // desktop header.
-    const header = isMobile && (history.location.pathname === '/reports/create' || history.location.pathname === '/tacoma/reports/create') ? null : isMobile ? <Header/> : <DesktopHeader/>;
+    const header =
+      isMobile &&
+      (history.location.pathname === "/reports/create" ||
+        history.location.pathname ===
+          "/tacoma/reports/create") ? null : isMobile ? (
+        <Header />
+      ) : (
+        <DesktopHeader />
+      );
     // If we are on mobile, and not on the create reports page, show a footer.
-    const footer = isMobile && history.location.pathname !== '/reports/create' ? <Footer/> : null;
+    const footer =
+      isMobile && history.location.pathname !== "/reports/create" ? (
+        <Footer />
+      ) : null;
     return (
       <div className="App">
         {header}
-        <Main/>
+        <Main />
         {footer}
       </div>
     );
@@ -52,7 +66,7 @@ class App extends Component {
 }
 
 const mapStateToProps = (state) => {
-  return {isMobile: state.isMobile};
+  return { isMobile: state.isMobile };
 };
 
 export default withRouter(connect(mapStateToProps)(App));
