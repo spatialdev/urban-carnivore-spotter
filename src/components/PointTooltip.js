1a67b94149a3ff96aeab5a137b00d593440ff421
Mealear Khiev <mealear.khiev@critigen.com>
2020-12-10 16:23:54 -0800

    Removes console statement

--- src/components/PointTooltip.js
+++ src/components/PointTooltip.js
@@ -1,51 +1,80 @@
-import React, {Component} from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import { withRouter } from 'react-router-dom';
-import {KeyboardArrowRight} from "@material-ui/icons";
-import Placeholder from '../assets/placeholder.svg';
+import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import { withRouter } from "react-router-dom";
+import { KeyboardArrowRight } from "@material-ui/icons";
+import Placeholder from "../assets/placeholder.svg";
 import { firebaseTimeToDateTimeString } from "../services/TimeService";
 
 const styles = {
-    allContent: {
-        display: 'flex',
-        height: 80,
-    },
-    caption: {
-        fontWeight: 'bold'
-    },
-    reportLink: {
-        height: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-    },
-    image: {
-        maxWidth: 40,
-        maxHeight: 40,
-        margin: 4
-    }
+  allContent: {
+    display: "flex",
+    height: 80,
+  },
+  caption: {
+    fontWeight: "bold",
+  },
+  reportLink: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  image: {
+    maxWidth: 40,
+    maxHeight: 40,
+    margin: 4,
+  },
 };
 
 class PointTooltip extends Component {
-    render() {
-        const { report, classes, history } = this.props;
-        console.log(report);
+  render() {
+    const { report, classes, history } = this.props;
+    const isInTacoma =
+      report.data !== undefined && report.data.isTacoma !== undefined
+        ? report.data.isTacoma
+        : false;
+    const path =
+      window.location.pathname.indexOf("tacoma") === -1
+        ? "/reports"
+        : "/tacoma/reports";
 
-        const isInTacoma = report.data!==undefined && report.data.isTacoma !==undefined ? report.data.isTacoma : false;
-        const path = window.location.pathname.indexOf('tacoma')===-1 ? '/reports' : '/tacoma/reports';
-        return <div className={classes.allContent}>
-            <img className={classes.image} src = {report.data.mediaPaths && report.data.mediaPaths.length>0 ? report.data.mediaPaths[0] : Placeholder}   alt=""/>
-            <div className={classes.info}>
-                <div><strong>{report.data.species}</strong></div>
-                <div><strong>Date & Time:</strong></div>
-                <div>{firebaseTimeToDateTimeString(report.data.timestamp)}</div>
-                <div><strong>Location:</strong> {report.data.neighborhood}</div>
-            </div>
-            <div className={classes.reportLink} style = {{cursor: "pointer"}} onClick={() => isInTacoma ? history.push(`${path}/tacoma/${report.id}`) : history.push(`${path}/${report.id}`) }>
-                <KeyboardArrowRight/>
-            </div>
+    return (
+      <div className={classes.allContent}>
+        <img
+          className={classes.image}
+          src={
+            report.data.mediaPaths && report.data.mediaPaths.length > 0
+              ? report.data.mediaPaths[0]
+              : Placeholder
+          }
+          alt=""
+        />
+        <div className={classes.info}>
+          <div>
+            <strong>{report.data.species}</strong>
+          </div>
+          <div>
+            <strong>Date & Time:</strong>
+          </div>
+          <div>{firebaseTimeToDateTimeString(report.data.timestamp)}</div>
+          <div>
+            <strong>Location:</strong> {report.data.neighborhood}
+          </div>
         </div>
-    }
-};
+        <div
+          className={classes.reportLink}
+          style={{ cursor: "pointer" }}
+          onClick={() =>
+            isInTacoma
+              ? history.push(`${path}/tacoma/${report.id}`)
+              : history.push(`${path}/${report.id}`)
+          }
+        >
+          <KeyboardArrowRight />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default withStyles(styles)(withRouter(PointTooltip));
