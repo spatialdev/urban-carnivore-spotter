62848d256897a69c2ef43702619e27d8a9eef35c
Mealear Khiev <mealear.khiev@critigen.com>
2020-12-11 13:02:22 -0800

    Updates all reports' neighborhoods in DB

--- src/services/ReportsService.js
+++ src/services/ReportsService.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const REPORTS_ENDPOINT =
   "https://us-central1-seattlecarnivores-edca2.cloudfunctions.net/getReports";
+// const UPDATE_REPORTS_ENDPOINT =
+//   "https://us-central1-seattlecarnivores-edca2.cloudfunctions.net/updateReports";
 
 export const getReports = () => {
   return axios
@@ -9,3 +11,11 @@ export const getReports = () => {
     .then((reports) => reports.data)
     .catch((error) => error);
 };
+
+// export const updateReports = (id, updatedNeighborhood) => {
+//   const updated = JSON.stringify(updatedNeighborhood);
+//   return axios
+//     .get(`${UPDATE_REPORTS_ENDPOINT}/?id=${id}&updatedNeighborhood=${updated}`)
+//     .then((res) => console.log("REPORTS UPDATED", JSON.stringify(res)))
+//     .catch((error) => error);
+// };
