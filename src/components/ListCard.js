ae036d1f5de4ef3d7c7dd2bc067d0a5bad4cc8e9
Mealear Khiev <mealear.khiev@critigen.com>
2020-12-17 18:45:43 -0800

    Makes ListCard responsive for mobile screens

--- src/components/ListCard.js
+++ src/components/ListCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import ReactPlayer from "react-player";
 import Card from "@material-ui/core/Card";
@@ -50,10 +51,29 @@ const styles = () => ({
       backgroundColor: "#ebe7e4",
     },
   },
+  seeReportMobile: {
+    display: "flex",
+    justifyContent: "center",
+    cursor: "pointer",
+    height: "15px",
+    width: "100px",
+    borderRadius: "4px",
+    backgroundColor: "#F6F4F3",
+    padding: "0.1em",
+    marginTop: "0.5rem",
+    "&:hover": {
+      backgroundColor: "#ebe7e4",
+    },
+  },
   seeReportText: {
     textAlign: "center",
     fontWeight: "bold",
     paddingTop: "0.1em",
+  },
+  seeReportTextMobile: {
+    fontSize: "0.75em",
+    padding: 0,
+    margin: 0,
   },
   title: {
     marginBottom: "0.75rem",
@@ -62,6 +82,13 @@ const styles = () => ({
     fontWeight: "bold",
     fontSize: "1.25em",
     letterSpacing: "0.56px",
+  },
+  titleMobile: {
+    fontSize: "0.9em",
+    fontWeight: "bold",
+    padding: 0,
+    margin: 0,
+    marginBottom: "0.5rem",
   },
   date: {
     display: "flex",
@@ -72,6 +99,11 @@ const styles = () => ({
     fontSize: "0.9em",
     fontWeight: 600,
   },
+  dateMobile: {
+    fontSize: "0.75em",
+    padding: 0,
+    margin: 0,
+  },
   location: {
     display: "flex",
     marginBottom: "0.75rem",
@@ -81,11 +113,20 @@ const styles = () => ({
     fontSize: "0.9em",
     fontWeight: 500,
   },
+  locationMobile: {
+    fontSize: "0.75em",
+    padding: 0,
+    margin: 0,
+  },
   icon: {
     marginRight: "0.5em",
   },
   staticMap: {
     cursor: "pointer",
+  },
+  staticMapMobile: {
+    height: "100px",
+    width: "100px",
   },
 });
 
@@ -100,28 +141,10 @@ class ListCard extends Component {
     });
   };
 
-  render() {
-    const { classes, handleReport, currReport } = this.props;
+  getStaticMap = () => {
+    const { classes, currReport, isMobile } = this.props;
     const lat = currReport.data.mapLat;
     const lng = currReport.data.mapLng;
-
-    let videoUrl;
-    let imageUrl = Placeholder;
-    let isVideo = false;
-    if (currReport.data.mediaPaths && currReport.data.mediaPaths.length > 0) {
-      const media = currReport.data.mediaPaths[0];
-      const fileExtensionPattern = /\.([0-9a-z]+)(?=[?#])|(\.)(?:[\w]+)$/gim;
-      const extension = media.match(fileExtensionPattern);
-      isVideo = videoFormats.includes(
-        extension ? extension[0].toLowerCase() : false
-      );
-
-      if (isVideo) {
-        videoUrl = media;
-      } else {
-        imageUrl = media;
-      }
-    }
 
     let color;
     const currSpecies = currReport.data.species;
@@ -145,52 +168,114 @@ class ListCard extends Component {
       color = "805b14";
     }
 
-    console.log("curr rep", currReport);
+    let w = !isMobile ? 200 : 100;
+    let h = !isMobile ? 200 : 150;
+    const staticReportMap = `https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/pin-s+${color}(${lng},${lat})/${lng},${lat},10,20/${w}x${h}?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`;
+    return (
+      <div
+        className={!isMobile ? classes.staticMap : classes.staticMapMobile}
+        onClick={this.handleMapView}
+      >
+        <img src={staticReportMap} />
+      </div>
+    );
+  };
+
+  renderMedia = () => {
+    const { currReport, isMobile } = this.props;
+    let videoUrl;
+    let imageUrl = Placeholder;
+    let isVideo = false;
+
+    if (currReport.data.mediaPaths && currReport.data.mediaPaths.length > 0) {
+      const media = currReport.data.mediaPaths[0];
+      const fileExtensionPattern = /\.([0-9a-z]+)(?=[?#])|(\.)(?:[\w]+)$/gim;
+      const extension = media.match(fileExtensionPattern);
+      isVideo = videoFormats.includes(
+        extension ? extension[0].toLowerCase() : false
+      );
+
+      if (isVideo) {
+        videoUrl = media;
+      } else {
+        imageUrl = media;
+      }
+    }
+
+    if (isVideo) {
+      let w = !isMobile ? 200 : 100;
+      let h = !isMobile ? 200 : 150;
+      return (
+        <ReactPlayer url={videoUrl} light={true} width={w} height={h} playing />
+      );
+    } else {
+      if (isMobile && imageUrl === Placeholder) {
+        return this.getStaticMap();
+      } else {
+        return <CardMedia className="cardPicture" image={imageUrl} />;
+      }
+    }
+  };
+
+  render() {
+    const { classes, handleReport, currReport, isMobile } = this.props;
     return (
       <Card className="card">
         <CardContent className={classes.allContent}>
-          {isVideo ? (
-            <ReactPlayer
-              url={videoUrl}
-              light={true}
-              width={200}
-              height={200}
-              playing
-            />
-          ) : (
-            <CardMedia className="cardPicture" image={imageUrl} />
-          )}
+          {this.renderMedia()}
           <CardContent className={classes.info}>
-            <Typography className={classes.title} variant={"h5"}>
+            <Typography
+              className={!isMobile ? classes.title : classes.titleMobile}
+              variant={"h5"}
+            >
               {currReport.data.species}
             </Typography>
-            <Typography variant={"subtitle1"} className={classes.date}>
+            <Typography
+              variant={"subtitle1"}
+              className={!isMobile ? classes.date : classes.dateMobile}
+            >
               <img className={classes.icon} src={dateIcon} alt="Date" />
               {firebaseTimeToDateTimeString(currReport.data.timestamp)}
             </Typography>
-            <Typography style={{ color: "grey" }} className={classes.location}>
+            <Typography
+              style={{ color: "grey" }}
+              className={!isMobile ? classes.location : classes.locationMobile}
+            >
               <img className={classes.icon} src={locationIcon} alt="Location" />
               {currReport.data.neighborhood
                 ? currReport.data.neighborhood
                 : "Washington"}
             </Typography>
             <div
-              className={classes.seeReport}
+              className={
+                !isMobile ? classes.seeReport : classes.seeReportMobile
+              }
               onClick={() => handleReport(currReport.id)}
             >
-              <div className={classes.seeReportText}>See Report</div>
-              <ChevronRightIcon />
+              <div
+                className={
+                  !isMobile
+                    ? classes.seeReportText
+                    : classes.seeReportTextMobile
+                }
+              >
+                See Report
+              </div>
+              {!isMobile && <ChevronRightIcon />}
             </div>
           </CardContent>
-          <div className={classes.staticMap} onClick={this.handleMapView}>
-            <img
-              src={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/pin-s+${color}(${lng},${lat})/${lng},${lat},10,20/200x200?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
-            />
-          </div>
+          {!isMobile && this.getStaticMap()}
         </CardContent>
       </Card>
     );
   }
 }
 
-export default withRouter(withStyles(styles)(ListCard));
+const mapStateToProps = (state) => {
+  return {
+    isMobile: state.isMobile,
+  };
+};
+export default withRouter(
+  connect(mapStateToProps)(withStyles(styles)(ListCard))
+);
